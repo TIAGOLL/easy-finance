ba7ec7d69028267242bd66c70987569a93cf85ac
Tiago Emanuel de Lima <tiagoepitanga10@gmail.com>
2025-01-16 13:12:44 -0300

    .

--- apps/web/src/forms/validations/update-task.ts
+++ apps/web/src/forms/validations/update-task.ts
@@ -1,6 +1,8 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const updateTaskSchema = z.object({
 	id: z.string(),
-	title: z.string().min(3, { message: 'A tarefa deve conter mais de 3 caracteres.' }),
-});
+	title: z
+		.string()
+		.min(3, { message: 'A tarefa deve conter mais de 3 caracteres.' }),
+})
